435849971f11c8b2b5d8f8e05efda2bb2b9296c7
mgtisantiago <mgtisantiago@gmail.com>
2023-11-16 18:18:33 -0600

    Version 1.0.8

--- js/script.js
+++ js/script.js
@@ -106,19 +106,19 @@ class Producto {
 
             {
                 clave: 6,
-                precio: 3500,
-                descuento: 700,
+                precio: 25000,
+                descuento: 500,
                 precio_final: 0,
-                nombre: "One Plus Nord N200",
+                nombre: "Canon EOS Rebel T100",
                 categoria: 2
             },
 
             {
                 clave: 7,
-                precio: 25000,
-                descuento: 500,
+                precio: 3500,
+                descuento: 700,
                 precio_final: 0,
-                nombre: "Canon EOS Rebel T100",
+                nombre: "One Plus Nord N200",
                 categoria: 2
             },
 
@@ -189,7 +189,6 @@ function catalogoProductos() {
     let productos = document.getElementById("productos");
     let catalogo;
     let listaProductos;
-    let claveImg = 0;
 
     OCategorias = Categoria.objetos();
     OProductos = Producto.objetos();
@@ -202,7 +201,6 @@ function catalogoProductos() {
             </header>
         <div class="row">`;
         listaProductos = "";
-        claveImg = 1;
         OProductos.forEach((pro) => {
             if (pro.categoria == cat.clave) {
                 catalogo = `
@@ -216,7 +214,7 @@ function catalogoProductos() {
                                         </div>
                                     </div>` : ``
                     }
-                        <img src="img/c${cat.clave}-${claveImg++}.png" class="card-img-top img-fluid">
+                        <img src="img/c${cat.clave}-${pro.clave}.png" class="card-img-top img-fluid">
                         </a>
                         <div class="card-body p-0 pt-3">
                             <div class="d-flex">
@@ -231,11 +229,11 @@ function catalogoProductos() {
                                 </span>` : `<span>&nbsp;</span>`
                     }
                             <p class="card-text mb-0">${pro.nombre}</p>
-                            <p id="btnAgregar-${pro.clave}">
+                            <div id="btnAgregar-${pro.clave}">
                                 <a href="javascript:agregar(${pro.clave})" class="btn btn-primary btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover">
                                     <i class="fas fa-plus fa-sm px-1 text-light"></i> Agregar
                                 </a>
-                            </p>
+                            </div>
                         </div>
                     </div>
                 </div>`;
@@ -253,12 +251,12 @@ function agregar(clave) {
     let botonAgregar = document.getElementById("btnAgregar-" + clave);
     let botones = `
     <div class="input-group w-50 mt-2 text-center align-items-center">
-        <a href="javascript:operaciones(${clave},${operacion = 1},${calcular="catalogo"})" class="btn btn-primary btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover ripple-surface-dark">
-            <i class="fas fa-plus fa-sm px-1 text-light"></i> 
-        </a>
-        <input id="cantidad-${clave}" type="text" min="0" max="15" readonly="readonly" class="form-control btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover text-center" value="1"/>
-        <a href="javascript:operaciones(${clave},${operacion = 2},${calcular="catalogo"})" class="btn btn-primary btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover ripple-surface-dark">
+        <a href="javascript:operaciones(${clave},${1},${1},${0})" class="btn btn-primary btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover ripple-surface-dark">
             <i class="fas fa-minus fa-sm px-1 text-light"></i> 
+        </a>   
+        <input id="cantidad-${clave}" type="text" min="0" max="15" readonly="readonly" class="form-control btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover text-center" value="1"/>
+        <a href="javascript:operaciones(${clave},${2},${1},${0})" class="btn btn-primary btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover ripple-surface-dark">
+            <i class="fas fa-plus fa-sm px-1 text-light"></i> 
         </a>
     </div>`;
     botonAgregar.innerHTML = botones;
@@ -282,48 +280,76 @@ function agregar(clave) {
     carrito.innerHTML = totalCarrito;
     let cantitadArticulos = document.getElementById("articulos");
     cantitadArticulos.innerHTML = totalArticulos;
+
+    console.log(aCarrito);
 }
 
-function operaciones(clave, operacion,calcular) {
+function operaciones(clave, operacion, calcular, eliminar) {
 
     total = 0;
     totalArticulos = 0;
     let cantidad = document.getElementById("cantidad-" + clave);
     let cantidadCarrito = document.getElementById("articulos-" + clave);
     let botonAgregar = document.getElementById("btnAgregar-" + clave);
+    let mensaje = document.getElementById("mensaje");
+    let restarCarrito="";
 
-    operacion == 1 ? cantidad.value = parseFloat(cantidad.value) + 1 : cantidad.value = parseFloat(cantidad.value) - 1;
+    if (calcular == 1) {
+        operacion == 1 ? cantidad.value = parseFloat(cantidad.value) - 1 : cantidad.value = parseFloat(cantidad.value) + 1;
+    } else {
+        operacion == 1 ? cantidad.value = parseFloat(cantidad.value) - 1 : cantidad.value = parseFloat(cantidad.value) + 1;
+        operacion == 1 ? cantidadCarrito.value = parseFloat(cantidadCarrito.value) - 1 : cantidadCarrito.value = parseFloat(cantidadCarrito.value) + 1;
+        restarCarrito = cantidadCarrito.value;
+    }
+
+   
     aCarrito.forEach((element, index) => {
+
         if (element.clave == clave) {
-            switch(calcular){
-                case "catalogo":
-                    element.subtotal = parseFloat(cantidad.value) * parseFloat(element.precioFinal);
-                    element.articulos = parseFloat(cantidad.value);
-                    if (cantidad.value == 0) {
-                        botonAgregar.innerHTML = ` 
-                        <p id="btnAgregar-${clave}">
+
+            if (calcular == 2 && eliminar == 0 && restarCarrito == 0 ) {
+                cantidad.value = 0;
+            }else if(eliminar == 1){
+                cantidad.value = 0;
+                cantidadCarrito.value = 0;
+                console.log("elimnar");
+            }
+
+            element.subtotal = parseFloat(cantidad.value) * parseFloat(element.precioFinal);
+            element.articulos = parseFloat(cantidad.value);
+
+            if (cantidad.value == 0) {
+                botonAgregar.innerHTML = ` 
                             <a href="javascript:agregar(${clave})" class="btn btn-primary btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover">
                                 <i class="fas fa-plus fa-sm px-1 text-light"></i> Agregar
-                            </a>
-                        </p>`;
-                        aCarrito.splice(index, 1);
-                        element.articulos = parseFloat(cantidad.value);
-                    }
-                break;
-                case "carrito":
-                    element.subtotal = parseFloat(cantidadCarrito.value) * parseFloat(element.precioFinal);
+                            </a>`;
+                eliminar == 0 && calcular == 1 ? aCarrito.splice(index, 1) : "";
+                element.articulos = parseFloat(cantidad.value);
+            }
+
+            if(calcular==2){
+
+                element.subtotal = parseFloat(cantidadCarrito.value) * parseFloat(element.precioFinal);
+                element.articulos = parseFloat(cantidadCarrito.value);
+
+                if (cantidadCarrito.value == 0) {
+                    mensaje.innerHTML = ` 
+                            <div id="sinCantidad" class="toast align-items-center text-bg-success border-0 position-fixed top-50 start-50 translate-middle p-3 text-light" role="alert" aria-live="assertive" aria-atomic="true">
+                                <div class="d-flex">
+                                    <div class="toast-body">
+                                        Eliminado: ${element.nombre}
+                                    </div>
+                                    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                                </div>
+                            </div>`;
+                    const toastSinCantidad = document.getElementById('sinCantidad');
+                    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastSinCantidad);
+                    toastBootstrap.show();
+                    aCarrito.splice(index, 1);
                     element.articulos = parseFloat(cantidadCarrito.value);
-                    if (cantidadCarrito.value == 0) {
-                        mensaje.innerHTML = ` 
-                        <p id="btnAgregar-${clave}">
-                            <a href="javascript:agregar(${clave})" class="btn btn-primary btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover">
-                                <i class="fas fa-plus fa-sm px-1 text-light"></i> Agregar
-                            </a>
-                        </p>`;
-                        aCarrito.splice(index, 1);
-                        element.articulos = parseFloat(cantidadCarrito.value);
-                    }
-                break;
+                    let articuloSinCantidad = document.getElementById(element.clave);
+                    articuloSinCantidad.remove();
+                }
             }
         }
     });
@@ -338,58 +364,61 @@ function operaciones(clave, operacion,calcular) {
     cantitadArticulos.innerHTML = totalArticulos;
 }
 
-function articulos(){
+function articulos() {
 
-    let listaArticulos= document.getElementById("productosCarrito");
+    let listaArticulos = document.getElementById("productosCarrito");
     let listaCarrito;
-    let claveImg = 1;
-
-    console.log(aCarrito);
+    listaArticulos.innerHTML="";
+    aCarrito.sort();
     
-        aCarrito.forEach((productos) => {
-            listaCarrito = `
+    aCarrito.forEach((productos) => {
+        listaCarrito = `
             
-                <div class="col-lg-4 col-md-6 col-sm-6">
+                <div id="${productos.clave}" class="col-lg-4 col-md-6 col-sm-6">
                     <div class="card my-2 shadow-0">
                         <a href="#" class="img-wrap">
                             ${productos.descuento > 0 ?
-                                `<div class="mask" style="height: 50px;">
+                `<div class="mask" style="height: 50px;">
                                     <div class="d-flex justify-content-start align-items-start h-100 m-2">
                                         <h6><span class="badge bg-success pt-2">Oferta</span></h6>
                                     </div>
                                 </div>` : ``
-                            }
-                            <img src="img/c${productos.categoria}-${claveImg++}.png" class="card-img-top img-fluid">
+            }
+                            <img src="img/c${productos.categoria}-${productos.clave}.png" class="card-img-top img-fluid">
                         </a>
                         <div class="card-body p-0 pt-3">
                             <div class="d-flex">
                                 <h5 class="card-title me-2">${productos.categoria == 1 ? formatoMoneda.format(productos.precioFinal) + "/pza" : formatoMoneda.format(productos.precioFinal)}</h5>
                                 ${productos.descuento > 0 ?
-                                    `<p class="text-muted text-decoration-line-through ">${productos.categoria == 1 ? formatoMoneda.format(productos.precio) + "/pza" : formatoMoneda.format(productos.precio)}
+                `<p class="text-muted text-decoration-line-through ">${productos.categoria == 1 ? formatoMoneda.format(productos.precio) + "/pza" : formatoMoneda.format(productos.precio)}
                                     </p>`: `<p>&nbsp;</p>`
-                                }
+            }
                             </div>
                             ${productos.descuento > 0 ?
-                                `<span class="badge badge-success py-2">Ahorra ${formatoMoneda.format(productos.descuento)}
+                `<span class="badge badge-success py-2">Ahorra ${formatoMoneda.format(productos.descuento)}
                                 </span>` : `<span>&nbsp;</span>`
-                            }
+            }
                             <p class="card-text mb-0">${productos.nombre}</p>
-                            <div class="input-group w-50 mt-2 text-center align-items-center">
-                                <a href="javascript:operaciones(${productos.clave},${operacion = 1},${calcular="carrito"})" class="btn btn-primary btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover ripple-surface-dark">
-                                    <i class="fas fa-plus fa-sm px-1 text-light"></i> 
-                                </a>
-                                <input id="cantidad-${productos.clave}" type="text" min="0" max="15" readonly="readonly" class="form-control btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover text-center" value="${productos.articulos}"/>
-                                <a href="javascript:operaciones(${productos.clave},${operacion = 1},${calcular="carrito"})" class="btn btn-primary btn-lg btn-rounded px-3 my-3 pt-2 float-start icon-hover ripple-surface-dark">
-                                    <i class="fas fa-minus fa-sm px-1 text-light"></i> 
-                                </a>
+                            <div id="btnAgregar-${productos.clave}">
+                                <div class="d-flex flex-row align-items-center mt-2">
+                                    <a href="javascript:operaciones(${productos.clave},${1},${2},${1})" class="btnEliminar my-3 py-1 me-4 float-start">
+                                        Eliminar
+                                    </a>
+                                    <div class="input-group text-center align-items-center">
+                                        <a id="${"sinCantidad-" + productos.clave}" href="javascript:operaciones(${productos.clave},${1},${2},${0})" class="btnCarrito px-3 my-3 py-1 float-start">
+                                            <i class="fas fa-minus fa-sm px-1"></i> 
+                                        </a>       
+                                        <input id="articulos-${productos.clave}" type="text" min="0" max="15" readonly="readonly" class="sinborde w-25 px-3 my-3 py-1 float-start text-center" value="${productos.articulos}"/>
+                                        <a href="javascript:operaciones(${productos.clave},${2},${2},${0})" class="btnCarrito px-3 my-3 py-1 float-start">
+                                            <i class="fas fa-plus fa-sm px-1"></i> 
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>`;
-            listaArticulos.innerHTML += listaCarrito;
-        });
-    
+        listaArticulos.innerHTML += listaCarrito;
+    });
+
 }
-
-
-
